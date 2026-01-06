360a0f7be666a755fe8f889e0324e915ed2c7c51
peytonjberger-arch <peytonjberger@gmail.com>
2026-01-06 08:41:06 -0500

    Add files via upload

---
+++ QB-Efficiency-EPAxCPOE.R
@@ -0,0 +1,47 @@
+library(nflfastR)
+library(tidyverse)
+library(ggrepel)
+
+
+
+pbp <- load_pbp(2025)
+
+
+# Define New DataFrame
+qbs <- pbp %>% 
+  filter(season_type == "REG", !is.na(epa)) %>% 
+  group_by(name, id) %>% 
+  summarise(
+    team = first(posteam),
+    epa_per_play = mean(epa),
+    cpoe_pass = mean(cpoe, na.rm = T),
+    dropbacks = sum(pass),
+    n_plays = n()
+  ) %>% 
+  filter(dropbacks > 250) %>% 
+  left_join(teams_colors_logos, by = c("team" = "team_abbr")) %>% 
+  arrange(desc(epa_per_play))
+
+
+# Create a Scatterplot
+ggplot(qbs)+
+  geom_point(aes(x = epa_per_play, y = cpoe_pass), color = qbs$team_color)+
+  geom_text_repel(aes(x = epa_per_play, y = cpoe_pass, label = name))+
+  geom_hline(yintercept = mean(qbs$cpoe_pass), color = "red", lty = "dashed")+
+  geom_vline(xintercept = mean(qbs$epa_per_play), color = "red", lty = "dashed")+
+  theme_bw()+
+  labs(
+    title = "Quarterback Efficiency: EPA/Play vs. CPOE",
+    subtitle = "2025 NFL Regular Season Performance (Min 250 Dropbacks)",
+    x = 'QB EPA per Play',
+    y = 'QB CPOE per Pass',
+    caption = 'By: Peyton Berger | Data: nflfastR'
+  )+
+  theme(plot.title = element_text(face = "bold", size = 14, hjust = 0.5))+
+  theme(plot.subtitle = element_text(size = 11, hjust = 0.5))
+  
+
+# Save Scatterplot
+ggsave("QB_Efficiency_EPAxCPOE.png", width = 15, height = 10, dpi = 'retina')
+  
+  
